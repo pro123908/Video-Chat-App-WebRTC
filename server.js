11f4492ad37c6de4f080b45a13ead37dfc9ee6be
Bilal Ahmad <pro123908@gmail.com>
2020-07-26 22:23:39 +0500

    Video Call app basic

--- server.js
+++ server.js
@@ -2,10 +2,28 @@ const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
+const { v4: uuidV4 } = require("uuid");
 
 app.set("view engine", "ejs");
-app.set(express.static("public"));
+app.use(express.static("public"));
 
-app.get("/", (req, res) => res.send("hello world"));
+app.get("/", (req, res) => res.redirect(`/${uuidV4()}`));
+
+app.get("/:roomId", (req, res) => {
+  res.render("room", { roomId: req.params.roomId });
+});
+
+io.on("connection", (socket) => {
+  socket.on("join", (roomId, userId) => {
+    console.log(roomId, userId);
+    socket.join(roomId);
+
+    socket.to(roomId).broadcast.emit("new-user", userId);
+
+    socket.on("disconnect", () => {
+      socket.to(roomId).broadcast.emit("user-disconnected", userId);
+    });
+  });
+});
 
 server.listen(5000, () => console.log("Server running on the port 5000"));
