3b42cc953cf5eb3bf3afffed3fc7717af996b22e
Bilal Ahmad <pro123908@gmail.com>
2020-07-27 01:49:11 +0500

    Flow Comments added

--- public/script.js
+++ public/script.js
@@ -1,33 +1,53 @@
+//client socket to interact with the server
 const socket = io("/");
+
 const videoGrid = document.getElementById("video-grid");
+
+// Peer js library - used to create new peer object
+// first param is id which is left up to server
+// host is the root route
+// port on which peer js server is running
 const myPeer = new Peer(undefined, {
   host: "/",
   port: "5001",
 });
 
+console.log("peer => ", myPeer);
+
+// creating video element to render your own video
 const myVideo = document.createElement("video");
+// muting it obviously you don't wanna hear yourself
 myVideo.muted = true;
 
+// array to keep track of all users connected
 const peers = {};
 
+// STEP-1
+// getting stream from your devices such as camera and microphone
 navigator.mediaDevices
   .getUserMedia({
     video: true,
     audio: true,
   })
   .then((stream) => {
+    // when connected to your devices, rendering video to the browser
+    // giving newly created video element and the stream from devices
     addVideoStream(myVideo, stream);
 
     myPeer.on("call", (call) => {
+      //connection from new user and then you answer with your own stream
       call.answer(stream);
       const video = document.createElement("video");
 
+      // getting the other user stream
       call.on("stream", (userVideoStream) => {
         addVideoStream(video, userVideoStream);
       });
     });
 
+    // Whenever a new user comes in your room
     socket.on("new-user", (userId) => {
+      console.log("New user connected with id => ", userId);
       connectToNewUser(userId, stream);
     });
   });
@@ -36,29 +56,58 @@ socket.on("user-disconnected", (userId) => {
   if (peers[userId]) peers[userId].close();
 });
 
+// STEP-1
+// when peer is opened
 myPeer.on("open", (id) => {
+  console.log("peer open => ", id);
   socket.emit("join", ROOM_ID, id);
 });
 
 function connectToNewUser(userId, stream) {
+  // giving that new user your stream
   const call = myPeer.call(userId, stream);
+
   const video = document.createElement("video");
+
+  // On stream from the new user
   call.on("stream", (userVideoStream) => {
     // Stream from the other user
+    // adding that stream in the video grid
     addVideoStream(video, userVideoStream);
   });
 
+  // closing/removing video  when stream is closed
   call.on("close", () => video.remove());
 
+  // adding new user call object in peers array
   peers[userId] = call;
 }
 
 function addVideoStream(video, stream) {
+  // setting source attribute of the video element to the stream to play
   video.srcObject = stream;
 
+  // when stream is ready to be displayed
   video.addEventListener("loadedmetadata", () => {
+    // playing the stream/video
     video.play();
   });
 
+  // and in last adding the video to the grid of the videos so that it can be seen
   videoGrid.append(video);
 }
+
+/* 
+ Flow of the communication
+
+ 1)Peer is opened and  your video from stream is displayed in your own browser first
+
+ 2) Then if new user comes the join event is called and so the method connectToNewUser is called with new userId and and your stream
+
+ 3) then the connection request is send to the new user with your stream and if the new user answers your request it return it's stream to you and then you simply render it in your browser
+
+ 4) and the other user render your stream in its browser
+
+ 
+ 
+*/
--- server.js
+++ server.js
@@ -7,23 +7,34 @@ const { v4: uuidV4 } = require("uuid");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+// Generating roomId whenever the root route is visited
+// and then redirected to the roomId route
 app.get("/", (req, res) => res.redirect(`/${uuidV4()}`));
 
+// Room ID route that render room ejs template
 app.get("/:roomId", (req, res) => {
   res.render("room", { roomId: req.params.roomId });
 });
 
+// socket is created whenever root route is visited
 io.on("connection", (socket) => {
+  // When client emit the join event
+  // userId and roomId are passed
   socket.on("join", (roomId, userId) => {
-    console.log(roomId, userId);
+    console.log("New join request => ", roomId + " ," + userId);
+    //Creating or joining the room with the specified room Id
     socket.join(roomId);
-
+    //Broadcasting new user joining message to all other nodes
+    // except the one that is newly connected
     socket.to(roomId).broadcast.emit("new-user", userId);
 
+    //Whenever window/connection is closed
     socket.on("disconnect", () => {
+      //Broadcasting  user exiting message to all other nodes
       socket.to(roomId).broadcast.emit("user-disconnected", userId);
     });
   });
 });
 
+// Running the server
 server.listen(5000, () => console.log("Server running on the port 5000"));
